d7c1cce39a7758a5c19f963bcb8de1ea1c4f9bd7
antomfdez <antonio.mesa.fdez@gmail.com>
2024-11-21 13:48:58 +0100

    Some fixes

--- deps-ins-arch.sh
+++ deps-ins-arch.sh
@@ -1,3 +1,3 @@
-pacman -Sy --noconfirm --needed ttf-font-awesome ttf-jetbrains-mono-nerd wofi ly pipewire pipewire-pulse pipewire-alsa pipewire-jack pipewire-media-session wireplumber starship swaybg foot dunst wlr-randr rofi wl-clipboard grim slurp river
+sudo pacman -Sy --noconfirm --needed ttf-font-awesome ttf-jetbrains-mono-nerd wofi ly pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber starship swaybg foot dunst wlr-randr rofi wl-clipboard grim slurp river
 
 yay -Sy --noconfirm --needed i3bar-river i3status-rust gohufont wideriver
